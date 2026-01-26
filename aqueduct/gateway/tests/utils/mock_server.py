6925b907a01bb18c1c16b9fff1d8b948b8519587
Natalia <natalia.maniakowska@tuwien.ac.at>
2026-01-26 12:52:53 +0100

    Separate streaming mock configs, remove some comments, update docstrings

--- aqueduct/gateway/tests/utils/mock_server.py
+++ aqueduct/gateway/tests/utils/mock_server.py
@@ -25,14 +25,11 @@ class MockConfig(BaseModel):
 
 
 class MockStreamingConfig(MockConfig):
-    response_data: list[bytes] = []  # ??
+    response_data: list[bytes] = []
     headers: Dict[str, str] = {"Content-Type": "text/event-stream"}
 
 
-default_mock_post_responses = {
-    # OK
-    "audio/speech": MockStreamingConfig(response_data=[b"mock", b"audio", b"data"]),
-    # OK
+default_post_configs = {
     "audio/transcriptions": MockConfig(
         response_data={
             "text": "This is a mock transcription",
@@ -80,7 +77,6 @@ default_mock_post_responses = {
             "usage": {"prompt_tokens": 10, "completion_tokens": 7, "total_tokens": 17},
         }
     ),
-    # OK
     "chat/completions": MockConfig(
         response_data={
             "id": "chatcmpl-123456789",
@@ -111,7 +107,6 @@ default_mock_post_responses = {
             },
         }
     ),
-    # OK
     "embeddings": MockConfig(
         response_data={
             "object": "list",
@@ -133,7 +128,6 @@ default_mock_post_responses = {
             "purpose": "fine-tune",
         }
     ),
-    # OK
     "images/generations": MockConfig(
         response_data={
             "created": 1713833628,
@@ -150,13 +144,13 @@ default_mock_post_responses = {
             },
         }
     ),
-    #
     "responses": MockConfig(
         response_data={
             "created_at": 1741476542,
             "completed_at": 1741476543,
             "id": "resp_67cb71b3c2b0819084d481baaaf148f206981a8637e6bc44",
             "max_output_tokens": 50,
+            "metadata": {},
             "model": "gpt-4.1-nano-2025-04-14",
             "object": "response",
             "output": [
@@ -191,12 +185,17 @@ default_mock_post_responses = {
                 "output_tokens_details": {"reasoning_tokens": 0},
                 "total_tokens": 30,
             },
-            "metadata": {},
         }
     ),
 }
 
-default_mock_get_responses = {
+default_post_stream_configs = {
+    # TODO: Add other stream responses; figure out a better way to retrieve them
+    "audio/speech": MockStreamingConfig(response_data=[b"mock", b"audio", b"data"]),
+    "responses": MockStreamingConfig(response_data=[b"..."]),
+}
+
+default_get_configs = {
     "responses/id": MockConfig(
         response_data={
             "completed_at": 1769125419,
@@ -259,7 +258,11 @@ default_mock_get_responses = {
     ),
 }
 
-special_mock_responses: dict[str, MockConfig] = {}
+# default_delete_configs = {
+#     "responses/id": MockConfig(response_data={'deleted': True})
+# }
+
+special_configs: dict[str, MockConfig] = {}
 
 
 @app.get("/health")
@@ -273,10 +276,10 @@ async def configure_endpoint(path: str, config: MockConfig | MockStreamingConfig
     Configure a special mock response for a specific endpoint.
 
     This endpoint allows to dynamically configure a mock response for any endpoint,
-    regardless of the request method (GET, POST). It can be used when the default
-    response from `default_mock_post_responses` is not what one needs.
+    regardless of the request method (GET, POST, DELETE). It can be used when the default
+    response from `default_{method}_configs` is not what one needs.
     """
-    special_mock_responses[path] = config
+    special_configs[path] = config
     return {"message": f"Configured a special mock response for {path}"}
 
 
@@ -288,27 +291,41 @@ async def reset_endpoint(path: str):
     A request to this endpoint should be sent after a test with a special mock response
     finishes, to prevent tests from interfering with one another.
     """
-    del special_mock_responses[path]
+    del special_configs[path]
     return {"message": f"Reset the special mock response for {path}"}
 
 
+# TODO: Do we need to mock any other GET/DELETE endpoints? Refactor the response
+#   matching to get rid of those if..elifs. Maybe create separate endpoints for
+#   GET and DELETE?
+# @app.delete("/{path:path}")
 @app.get("/{path:path}")
 @app.post("/{path:path}")
 async def mock_endpoint(path: str, request: Request):
     path = path.strip("/").removeprefix("v1/")
 
     try:
-        if path in special_mock_responses:
-            config = special_mock_responses[path]
+        if path in special_configs:
+            # Special config can be a streaming or a JSON one.
+            config = special_configs[path]
+        elif _should_stream(request):
+            # Streaming configs are stored in a separate dict, because some
+            # endpoints can return both a streaming or a JSON response.
+            config = default_post_stream_configs[path]
         elif request.method == "POST":
-            config = default_mock_post_responses[path]
-        else:  # request.method == "GET"
+            config = default_post_configs[path]
+        elif request.method == "GET":
             if re.match("^responses/.+/input_items$", path):
-                config = default_mock_get_responses["responses/id/input_items"]
+                config = default_get_configs["responses/id/input_items"]
             elif re.match("responses/.+$", path):
-                config = default_mock_get_responses["responses/id"]
+                config = default_get_configs["responses/id"]
             else:
-                config = default_mock_get_responses[path]
+                config = default_get_configs[path]
+        # elif request.method == "DELETE":
+        #     if re.match("^responses/.+$", path):
+        #         config = default_delete_configs["responses/id"]
+        #     else:
+        #         config = default_delete_configs[path]
     except KeyError:
         raise HTTPException(
             status_code=HTTP_404_NOT_FOUND, detail=f"No mock configured for this endpoint: {path}"
@@ -321,6 +338,15 @@ async def mock_endpoint(path: str, request: Request):
     return JSONResponse(
         content=config.response_data, status_code=config.status_code, headers=config.headers
     )
+
+
+async def _should_stream(request: Request) -> bool:
+    try:
+        request_data = await request.json()
+        should_stream = request_data.get("stream") is True
+    except Exception:
+        should_stream = False
+    return should_stream
 
 
 class MockAPIServer:

7fddcf279f3584c6baf1a9e5d5c6d5e6ce2bf4f4
Matt, Matthias <matthias.matt@tuwien.ac.at>
2026-03-16 15:12:24 +0100

    allow batch cancel from ui better ui error message for batch cancel and file delete

--- aqueduct/gateway/views/batches.py
+++ aqueduct/gateway/views/batches.py
@@ -201,7 +201,7 @@ async def batch(request: ASGIRequest, token: Token, batch_id: str, *args, **kwar
 
 @csrf_exempt
 @require_POST
-@token_authenticated(token_auth_only=True)
+@token_authenticated(token_auth_only=False)
 @log_request
 async def batch_cancel(request: ASGIRequest, token: Token, batch_id: str, *args, **kwargs):
     """

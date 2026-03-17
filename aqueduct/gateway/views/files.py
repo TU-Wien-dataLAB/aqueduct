cbbc02e9b7c77ce7cdd69e9c424b535482d2d94a
Natalia <natalia.maniakowska@tuwien.ac.at>
2026-03-17 14:25:34 +0100

    Add better type annotations in `files` and `transcriptions` views

--- aqueduct/gateway/views/files.py
+++ aqueduct/gateway/views/files.py
@@ -11,6 +11,7 @@ from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_http_methods
 from openai import AsyncOpenAI
+from openai.types.file_create_params import FileCreateParams as OpenAIFileCreateParams
 from pydantic import BaseModel, ConfigDict, TypeAdapter
 
 from gateway.config import get_files_api_client
@@ -29,6 +30,7 @@ from .errors import error_response
 class FilesCreateParams(BaseModel):
     file: bytes
     purpose: Literal["assistants", "batch", "user_data"]
+    # IO[bytes] requires arbitrary_types_allowed for model settings
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
@@ -134,7 +136,7 @@ async def sync_batch_file_if_needed(
 async def files(
     request: ASGIRequest,
     token: Token,
-    pydantic_model: Optional[dict] = None,
+    pydantic_model: Optional[OpenAIFileCreateParams] = None,
     file_content: Optional[bytes] = None,
     file_preview: Optional[str] = None,
     *args,

c6068525deae3b7a0c2a02ead69fe4e716f9f8d3
Matt, Matthias <matthias.matt@tuwien.ac.at>
2026-03-23 17:19:14 +0100

    Undo ADMIN_GROUP push

--- aqueduct/aqueduct/settings.py
+++ aqueduct/aqueduct/settings.py
@@ -123,7 +123,7 @@ def my_org_name_extractor(groups: list[str]) -> str | None:
 
 OIDC_DEFAULT_GROUPS = ["default"]
 ORG_NAME_FROM_OIDC_GROUPS_FUNCTION = lambda x: "default"
-ADMIN_GROUP = "aqueduct-admin"  # all users are admins
+ADMIN_GROUP = "default"  # all users are admins
 
 EXTRA_NAV_LINKS = {
     "Bug Report": "https://github.com/TU-Wien-dataLAB/aqueduct/issues/new?template=bug_report.md",

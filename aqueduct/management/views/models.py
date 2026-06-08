8703d8ada88fdf475bf4c66b392e9a5b49d01cad
Matt, Matthias <matthias.matt@tuwien.ac.at>
2026-06-08 12:01:28 +0200

    use cards for model list similar to mcp server list

--- aqueduct/management/views/models.py
+++ aqueduct/management/views/models.py
@@ -27,4 +27,5 @@ class ModelListView(LoginRequiredMixin, TemplateView):
 
         context["title"] = "Models"
         context["model_list"] = model_list
+        context["site_host"] = f"{self.request.scheme}://{self.request.get_host()}"
         return context

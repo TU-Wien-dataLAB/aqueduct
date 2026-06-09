517be080b0ae7d25309a5d1ade8f25bc689ec590
Matt, Matthias <matthias.matt@tuwien.ac.at>
2026-06-09 16:31:49 +0200

    Revert "file and batch expires-at badges in ui"
    This reverts commit 75df5d2a3117ad838785b39990181170f482ea84.

--- aqueduct/management/views/files.py
+++ aqueduct/management/views/files.py
@@ -1,7 +1,6 @@
-from datetime import UTC, datetime
+from datetime import UTC
 
 from django.db.models import Q
-from django.utils import timezone
 from django.views.generic import TemplateView
 
 from management.models import FileObject
@@ -25,22 +24,11 @@ class UserFilesView(BaseAqueductView, TemplateView):
             Q(token__user=user) | Q(token__service_account__team__in=teams)
         ).order_by("-created_at")
 
-        now_ts = int(timezone.now().timestamp())
+        # Convert Unix timestamp to datetime for template date filter
+        from datetime import datetime
 
         for f in files:
             f.created_dt = datetime.fromtimestamp(f.created_at, tz=UTC)
-            # Convert expires_at for template display
-            if f.expires_at is not None:
-                f.expires_dt = datetime.fromtimestamp(f.expires_at, tz=UTC)
-                if f.expires_at <= now_ts:
-                    f.expiration_status = "expired"
-                elif f.expires_at <= now_ts + 86400 * 14:  # within 2 weeks
-                    f.expiration_status = "expiring_soon"
-                else:
-                    f.expiration_status = "active"
-            else:
-                f.expires_dt = None
-                f.expiration_status = "never"
 
         context["files"] = files
         return context
